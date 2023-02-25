940179b40e30281077f10f85d44d63f35c05684a
Hemant Kumar Singh <iamhemant.singh14@gmail.com>
2023-02-25 18:48:47 -0500

    added new word functinality

--- app/src/main/java/com/example/hangman/ChooseLetterFragment.kt
+++ app/src/main/java/com/example/hangman/ChooseLetterFragment.kt
@@ -1,5 +1,7 @@
 package com.example.hangman
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment
 class ChooseLetterFragment(val gameFragment: GameFragment) : Fragment() {
     private lateinit var letterButtons: Array<Button>
     private lateinit var hintButton: Button
+    private lateinit var restartButton: Button
     private var hintCount = 0
     //val gameFragment = GameFragment()
 
@@ -21,7 +24,16 @@ class ChooseLetterFragment(val gameFragment: GameFragment) : Fragment() {
 //        }
 //    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private var listener: RestartListener? = null
+
+    interface RestartListener {
+        fun restartApp()
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_choose_letter, container, false)
 
         letterButtons = arrayOf(
@@ -59,7 +71,23 @@ class ChooseLetterFragment(val gameFragment: GameFragment) : Fragment() {
         }
         hintButton = view.findViewById(R.id.button_hint)
         hintButton.setOnClickListener { showHint() }
+
+        restartButton = view.findViewById(R.id.button_new_word)
+        restartButton.setOnClickListener {
+            listener?.restartApp()
+        }
+
         return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as RestartListener
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
     }
 
     private fun selectLetter(button: Button) {
@@ -75,63 +103,68 @@ class ChooseLetterFragment(val gameFragment: GameFragment) : Fragment() {
 
             gameFragment.checkLetter(button.text.toString())
             gameFragment.useTurn()
-        }
-        else{
+        } else {
             println("----------------did not got GameFragment as parentFragment")
         }
     }
+
     private fun showHint() {
 //        val gameFragment = parentFragment as GameFragment
         when (hintCount) {
             0 -> {
                 // Display the hint message
-                if(gameFragment.chosenWord==getString(R.string.a)){
-                    gameFragment.showHint("What do you do with a book?.")}
+                if (gameFragment.chosenWord == getString(R.string.a)) {
+                    gameFragment.showHint("What do you do with a book?.")
+                }
 
-                if(gameFragment.chosenWord==getString(R.string.b)){
-                    gameFragment.showHint("This is a mobile operating system")}
+                if (gameFragment.chosenWord == getString(R.string.b)) {
+                    gameFragment.showHint("This is a mobile operating system")
+                }
 
-                if(gameFragment.chosenWord==getString(R.string.c)){
-                    gameFragment.showHint("It is a kind of sibling.")}
+                if (gameFragment.chosenWord == getString(R.string.c)) {
+                    gameFragment.showHint("It is a kind of sibling.")
+                }
 
-                if(gameFragment.chosenWord==getString(R.string.d)){
-                    gameFragment.showHint("This is used in expensive jewelry")}
+                if (gameFragment.chosenWord == getString(R.string.d)) {
+                    gameFragment.showHint("This is used in expensive jewelry")
+                }
 
-                if(gameFragment.chosenWord==getString(R.string.e)){
-                    gameFragment.showHint("What do you get when you come out of prison.")}
+                if (gameFragment.chosenWord == getString(R.string.e)) {
+                    gameFragment.showHint("What do you get when you come out of prison.")
+                }
 
 
             }
             1 -> {
                 // Disable half of the remaining letter buttons
                 val remainingLetters = gameFragment.getRemainingLetters()
-                println("---------------showHint-remainingLetters: "+ remainingLetters)
+                println("---------------showHint-remainingLetters: " + remainingLetters)
 
                 // get length of enabled buttons and put it in buttonsEnabled list
-                var disableCount=0
-                var buttonsEnabled : MutableList<Button> = mutableListOf()
-                for (button in letterButtons){
-                    if (button.isEnabled==true){
-                        disableCount+=1
+                var disableCount = 0
+                var buttonsEnabled: MutableList<Button> = mutableListOf()
+                for (button in letterButtons) {
+                    if (button.isEnabled == true) {
+                        disableCount += 1
                         buttonsEnabled.add(button)
                     }
                 }
 
                 disableCount /= 2
 
-                println("-------------show hint func disableCount: "+ disableCount)
+                println("-------------show hint func disableCount: " + disableCount)
 
                 var disabledCount = 0
                 val size = buttonsEnabled.size
 
-                println("-------------show hint func letters: "+ letterButtons)
+                println("-------------show hint func letters: " + letterButtons)
                 for (i in 0 until size) {
 
                     //if (button.isEnabled &&  !remainingLetters.contains(button.text.toString())) {
                     var button = buttonsEnabled.random()
-                    println("-------------show hint func letters: "+ button.text.toString())
-                    if (button.isEnabled &&  (button.text.toString() !in remainingLetters.toString())) {
-                        println("-------------show hint func button.text.toString(): "+ button.text.toString())
+                    println("-------------show hint func letters: " + button.text.toString())
+                    if (button.isEnabled && (button.text.toString() !in remainingLetters.toString())) {
+                        println("-------------show hint func button.text.toString(): " + button.text.toString())
                         button.isEnabled = false
                         disabledCount++
                         if (disabledCount >= disableCount) break
@@ -143,9 +176,9 @@ class ChooseLetterFragment(val gameFragment: GameFragment) : Fragment() {
             }
             2 -> {
                 // Disable all vowel buttons and show the vowels
-                var buttonsEnabled : MutableList<Button> = mutableListOf()
-                for (button in letterButtons){
-                    if (button.isEnabled==true){
+                var buttonsEnabled: MutableList<Button> = mutableListOf()
+                for (button in letterButtons) {
+                    if (button.isEnabled == true) {
 
                         buttonsEnabled.add(button)
                     }
