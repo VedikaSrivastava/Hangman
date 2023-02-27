b64448e71e179cab8603e6118d0aa3af688624ad
pnarne148 <115562106+pnarne148@users.noreply.github.com>
2023-02-27 03:45:24 -0500

    bug fix

--- app/src/main/java/com/example/hangman/GameFragment.kt
+++ app/src/main/java/com/example/hangman/GameFragment.kt
@@ -25,7 +25,7 @@ class GameFragment : Fragment() {
     private var remainingTurns = 0
     var hintCount = 0
     var lettersClicked: MutableSet<String> = mutableSetOf()
-    var timer = 30000
+    var timer = 10000
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_game, container, false)
@@ -85,7 +85,8 @@ class GameFragment : Fragment() {
             // when the time is up
             override fun onFinish() {
                 timerView.setText("Time Up!")
-                Toast.makeText(requireContext(), "Time up!!", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(requireContext(), "Time up!!", Toast.LENGTH_SHORT).show()
+                //TODO: Context not working
             }
         }.start()
     }
