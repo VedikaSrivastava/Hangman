5deb4ea3ee40f25d0fae188b9b40df96a8ae2fd5
Hemant Kumar Singh <iamhemant.singh14@gmail.com>
2023-02-28 18:39:03 -0500

    choose frag test

--- app/src/androidTest/java/com/example/hangman/ChooseLetterInstrumentTest.kt
+++ app/src/androidTest/java/com/example/hangman/ChooseLetterInstrumentTest.kt
@@ -1,96 +1,26 @@
 package com.example.hangman
 
-import android.view.View
-import android.widget.Button
-import junit.framework.TestCase.*
-import kotlinx.android.synthetic.main.fragment_choose_letter.*
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.hangman.MainActivity
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class ChooseLetterFragmentTest {
+@RunWith(AndroidJUnit4::class)
+class ChooseLetterInstrumentTest {
 
-    @Test
-    fun test_selectLetter_buttonDisabledOnClick() {
-        // Arrange
-        val fragment = ChooseLetterFragment()
-        val button = Button(fragment.context)
-        button.isEnabled = true
-        button.isClickable = true
-
-        // Act
-        fragment.selectLetter(button)
-
-        // Assert
-        assertFalse(button.isEnabled)
-        assertFalse(button.isClickable)
-    }
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun test_selectLetter_letterAddedToClickedLetters() {
-        // Arrange
-        val fragment = ChooseLetterFragment()
-        val button = Button(fragment.context)
-        button.text = "A"
+    fun clickButtonAndCheckTextView() {
+        Espresso.onView(ViewMatchers.withId(R.id.button_a))
+            .perform(ViewActions.click())
 
-        // Act
-        fragment.selectLetter(button)
-
-        // Assert
-        assertTrue(fragment.lettersClicked.contains(button.text))
     }
-
-//    @Test
-//    fun test_checkWinCondition_gameWon() {
-//        // Arrange
-//        val fragment = ChooseLetterFragment()
-//        val gameFragment = GameFragment()
-//        gameFragment.chosenWord = "APPLE"
-//        gameFragment.currentWord = "APPLE"
-//        fragment.gameFragment = gameFragment
-//
-//        // Act
-//        fragment.checkWinCondition()
-//
-//        // Assert
-//        assertEquals(View.VISIBLE, fragment.resultLayout.visibility)
-//        assertEquals(View.VISIBLE, fragment.wonView.visibility)
-//        assertEquals(View.GONE, fragment.choose.visibility)
-//        assertEquals(View.GONE, fragment.keyboard.visibility)
-//    }
-//
-//    @Test
-//    fun test_checkWinCondition_gameLost() {
-//        // Arrange
-//        val fragment = ChooseLetterFragment()
-//        val gameFragment = GameFragment()
-//        gameFragment.chosenWord = "APPLE"
-//        gameFragment.currentWord = "_____" // all turns used up
-//        fragment.gameFragment = gameFragment
-//
-//        // Act
-//        fragment.checkWinCondition()
-//
-//        // Assert
-//        assertEquals(View.VISIBLE, fragment.resultLayout.visibility)
-//        assertEquals(View.VISIBLE, fragment.lostView.visibility)
-//        assertEquals(View.GONE, fragment.choose.visibility)
-//        assertEquals(View.GONE, fragment.keyboard.visibility)
-//    }
-//
-//    @Test
-//    fun test_showHint_hintDisplayed() {
-//        // Arrange
-//        val fragment = ChooseLetterFragment()
-//        val gameFragment = GameFragment()
-//        gameFragment.chosenWord = "APPLE"
-//        //gameFragment.currentWord = "_____" // all turns used up
-//        fragment.gameFragment = gameFragment
-//        val expectedHint = "What do you do with a book?."
-//
-//        // Act
-//        fragment.showHint()
-//
-//        // Assert
-//        assertEquals(expectedHint, gameFragment.hint)
-//    }
 }
-
